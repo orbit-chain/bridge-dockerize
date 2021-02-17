5d4dd92f8d70ebcfd314fe2eee59c5523ac5c349
JayB <jayb@dxmcorp.com>
2021-02-17 15:52:44 +0900

    fix bug

--- nodes/validator/src/klaytn/v2/utils/packer.js
+++ nodes/validator/src/klaytn/v2/utils/packer.js
@@ -9,6 +9,24 @@ exports.packSwapData = function(data) {
     result.push({t: 'bytes', v: toHexBuffer(data.token)});
     result.push({t: 'bytes32[]', v: data.bytes32s});
     result.push({t: 'uint[]', v: data.uints});
+    result.push({t: 'bytes', v: toHexBuffer(data.data)});
+
+    return result;
+};
+
+exports.packSwapNFTData = function(data) {
+    let result = [];
+
+    result.push({t: 'string', v: "NFT"});
+    result.push({t: 'address', v: data.hubContract});
+    result.push({t: 'string', v: data.fromChain});
+    result.push({t: 'string', v: data.toChain});
+    result.push({t: 'bytes', v: toHexBuffer(data.fromAddr)});
+    result.push({t: 'bytes', v: toHexBuffer(data.toAddr)});
+    result.push({t: 'bytes', v: toHexBuffer(data.token)});
+    result.push({t: 'bytes32[]', v: data.bytes32s});
+    result.push({t: 'uint[]', v: data.uints});
+    result.push({t: 'bytes', v: toHexBuffer(data.data)});
 
     return result;
 };
