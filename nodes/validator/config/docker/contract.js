4a9135da2afe4dab9395b1c9c26fd7f136dfc97a
LeeJeongUk <ljw2421@ozys.net>
2021-07-23 14:50:40 +0900

    remove terra feature

--- nodes/validator/config/docker/contract.js
+++ nodes/validator/config/docker/contract.js
@@ -22,8 +22,6 @@ module.exports = {
     ORBIT_HUB_MULTISIG: settings.BridgeAddress.MessageMultiSigWallet.Hub,
     ETH_BRIDGE_CONTRACT: settings.BridgeAddress.EthBridgeContract,
     ETH_BRIDGE_MULTISIG: settings.BridgeAddress.MessageMultiSigWallet.Eth,
-    TERRA_BRIDGE_CONTRACT: settings.BridgeAddress.TerraBridgeContract,
-    TERRA_BRIDGE_MULTISIG: settings.BridgeAddress.MessageMultiSigWallet.Terra,
     KLAYTN_BRIDGE_CONTRACT: settings.BridgeAddress.KlaytnBridgeContract,
     KLAYTN_BRIDGE_MULTISIG: settings.BridgeAddress.MessageMultiSigWallet.Klaytn,
     ICON_BRIDGE_CONTRACT: settings.BridgeAddress.IconBridgeContract,
