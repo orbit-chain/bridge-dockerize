a7e01b05f15d67eb8126f7663e201ff6f19a25d3
LeeJeongUkk <ljw2421@ozys.net>
2022-07-29 10:03:15 +0900

    check klaytn endpoints before init

--- nodes/validator/config/docker/klaytn.js
+++ nodes/validator/config/docker/klaytn.js
@@ -1,8 +1,8 @@
 const settings = require("./settings");
 
 module.exports = {
-    KLAYTN_ISKAS: settings.Endpoints.Klaytn.isKas,
-    KLAYTN_RPC: settings.Endpoints.Klaytn.rpc,
-    KLAYTN_WS: settings.Endpoints.Klaytn.socket,
-    KLAYTN_KAS: settings.Endpoints.Klaytn.Kas
+    KLAYTN_ISKAS: settings.Endpoints.Klaytn && settings.Endpoints.Klaytn.isKas,
+    KLAYTN_RPC: settings.Endpoints.Klaytn && settings.Endpoints.Klaytn.rpc,
+    KLAYTN_WS: settings.Endpoints.Klaytn && settings.Endpoints.Klaytn.socket,
+    KLAYTN_KAS: settings.Endpoints.Klaytn && settings.Endpoints.Klaytn.Kas
 };
